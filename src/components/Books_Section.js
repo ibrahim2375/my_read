28c915b1c57e38da166a8d7bd3609006183488b6
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-08-05 23:02:43 +0200

    addin search

---
+++ src/components/Books_Section.js
@@ -0,0 +1,49 @@
+// MUI icons
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+function Books_Section({ Books, Remove_Book, Change_List, section_data }) {
+    const handleBook = (event, id) => {
+        const selected_value = event.target.value;
+        if (selected_value === 'none') {
+            Remove_Book(id);
+        }
+        else if (selected_value === 'currently_reading') {
+            Change_List(id, 'currently_read');
+        }
+        else if (selected_value === 'read') {
+            Change_List(id, 'read');
+        } else {
+            Change_List(id, 'want_to_read');
+        }
+    }
+    return (
+        <ol className="books">
+            {
+                Books.filter((book) => book.state === section_data).map((cr) => (
+                    <li className="book" key={cr.id} >
+                        <div className="image">
+                            <img src={cr.book_img} alt="" />
+                            <div className="select_menu">
+                                <div className="select_icon">
+                                    <div className="arrow_icon">
+                                        <ArrowDropDownIcon sx={{ color: 'white', fontSize: '32px' }} />
+                                    </div>
+                                    <select value={cr.state} onChange={(e) => handleBook(e, cr.id)}>
+                                        <option value="currently_reading">currently reading</option>
+                                        <option value="read">read</option>
+                                        <option value="want to read">want to read</option>
+                                        <option value="none">none</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="book_details">
+                            <h4>{cr.book_name}</h4>
+                            <p>{cr.book_author}</p>
+                        </div>
+                    </li>
+                ))
+            }
+        </ol>
+    )
+}
+export default Books_Section;
---
+++ src/pages/Search.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+//icons
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+//router
+import { Link } from 'react-router-dom'
+function Search({ books, remove_book, change_list }) {
+    const [searchInput, setsearchInput] = useState('');
+    const handle = (event) => {
+        setsearchInput(event.trim());
+    }
+    //search
+    const result = books.filter((B) => B.book_name.toLowerCase().includes(searchInput.toLowerCase()));
+    ///handel
+    const handleBook = (event, id) => {
+        const selected_value = event.target.value;
+        if (selected_value === 'none') {
+            remove_book(id);
+        }
+        else if (selected_value === 'currently_reading') {
+            change_list(id, 'currently_read');
+        }
+        else if (selected_value === 'read') {
+            change_list(id, 'read');
+        }
+        else {
+            change_list(id, 'want_to_read');
+        }
+    }
+    return (
+        <div className="search">
+            <input type="text" placeholder="Search" value={searchInput} onChange={(e) => handle(e.target.value)} />
+            <div className="search_page_content">
+                <Link to="/">
+                    <KeyboardBackspaceIcon sx={{ color: 'green', fontSize: '32px' }} />
+                </Link>
+                <div className="serch_result">
+                    <ol className="books">
+                        {
+                            result.map((cr) => (
+                                <li className="book" key={cr.id} >
+                                    <div className="image">
+                                        <img src={cr.book_img} alt="" />
+                                        <div className="select_menu">
+                                            <div className="select_icon">
+                                                <div className="arrow_icon">
+                                                    <ArrowDropDownIcon sx={{ color: 'white', fontSize: '32px' }} />
+                                                </div>
+                                                <select value={cr.state} onChange={(e) => handleBook(e, cr.id)}>
+                                                    <option value="currently_reading">currently reading</option>
+                                                    <option value="read">read</option>
+                                                    <option value="want_to_read">want to read</option>
+                                                    <option value="none">none</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="book_details">
+                                        <h4>{cr.book_name}</h4>
+                                        <p>{cr.book_author}</p>
+                                    </div>
+                                </li>
+                            ))
+                        }
+                    </ol>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search
