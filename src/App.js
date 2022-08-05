fc150ed13e05035171c865773416ebcac7e571d1
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-08-05 23:02:33 +0200

    Home design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './css/App.css';
+import { useState } from 'react'
+//import books data
+import { Books } from './BooksData'
+//MUI icons
+import AddIcon from '@mui/icons-material/Add';
+//components
+import Home from './pages/Home'
+import Search from './pages/Search'
+//router
+import { Routes, Route, Link } from 'react-router-dom'
 function App() {
+  const [books, setbooks] = useState(Books)
+  const remove_book = (id) => {
+    setbooks(books.filter((b) => b.id !== id));
+  }
+  const change_list = (id, state) => {
+    setbooks(books.map((b) => b.id === id ? { ...b, state: state } : b));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<Home remove_book={remove_book} change_list={change_list} books={books} />}>
+        </Route>
+        <Route path="/search" element={<Search remove_book={remove_book} change_list={change_list} books={books} />}>
+        </Route>
+      </Routes>
+      <Link to='/search' className="Add_books">
+        <AddIcon sx={{ color: 'white', fontSize: '32px' }} />
+      </Link>
     </div>
   );
 }
