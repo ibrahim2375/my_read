2767a0f529382fc7c8c9038203f0078c67ccfba5
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-08-11 15:51:27 +0200

    adding book component

---
+++ src/components/Book.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react'
+//porp types
+import PropTypes from 'prop-types'
+// MUI icons
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+function Book({ imageLinks, title, authors, Change_List, shelf, book }) {
+    const [shelves] = useState([
+        { id: 1, shelfName: 'currentlyReading', shelfDisplayName: 'Currently Reading' },
+        { id: 2, shelfName: 'read', shelfDisplayName: 'read' },
+        { id: 3, shelfName: 'wantToRead', shelfDisplayName: 'want to read' },
+        { id: 4, shelfName: 'none', shelfDisplayName: 'none' },
+    ])
+    const handleBook = (event, book) => {
+        const selected_value = event.target.value;
+        Change_List(book, selected_value);
+    }
+
+    return (
+        <li className="book"  >
+            <div className="image">
+                <img src={imageLinks} alt="not available" />
+                <div className="select_menu">
+                    <div className="select_icon">
+                        <div className="arrow_icon">
+                            <ArrowDropDownIcon sx={{ color: 'white', fontSize: '32px' }} />
+                        </div>
+                        <select value={shelf} onChange={(e) => handleBook(e, book)}>
+                            <option value='move' disabled>move to..</option>
+                            {
+                                shelves.map(shelf => (
+                                    <option key={shelf.id} value={shelf.shelfName} >{shelf.shelfDisplayName}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="book_details">
+                <h4>{title}</h4>
+                <p>{authors}</p>
+            </div>
+        </li>
+    )
+}
+Book.prototype = {
+    imageLinks: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    authors: PropTypes.array.isRequired,
+    Change_List: PropTypes.func.isRequired,
+    shelf: PropTypes.string.isRequired,
+    book: PropTypes.object.isRequired,
+}
+export default Book
--- src/components/Books_Section.js
+++ src/components/Books_Section.js
@@ -1,38 +1,14 @@
 //porp types
 import PropTypes from 'prop-types'
-// MUI icons
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Book from './Book'
+
 function Books_Section({ Books, Change_List, section_data }) {
-    const handleBook = (event, book) => {
-        const selected_value = event.target.value;
-        Change_List(book, selected_value);
-    }
     return (
         <ol className="books">
             {
-                Books.filter((book) => book.shelf === section_data).map((cr) => (
-                    <li className="book" key={cr.id} >
-                        <div className="image">
-                            <img src={cr.imageLinks.thumbnail} alt="book" />
-                            <div className="select_menu">
-                                <div className="select_icon">
-                                    <div className="arrow_icon">
-                                        <ArrowDropDownIcon sx={{ color: 'white', fontSize: '32px' }} />
-                                    </div>
-                                    <select value={cr.shelf} onChange={(e) => handleBook(e, cr)}>
-                                        <option value="currentlyReading">currently reading</option>
-                                        <option value="read">read</option>
-                                        <option value="wantToRead">want to read</option>
-                                        <option value="none">none</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="book_details">
-                            <h4>{cr.title}</h4>
-                            <p>{cr.authors[0]}</p>
-                        </div>
-                    </li>
+                Books.filter((book) => book.shelf === section_data).map((b) => (
+                    <Book key={b.id} imageLinks={b.imageLinks.thumbnail} title={b.title} authors={b.authors} Change_List={Change_List} shelf={b.shelf} book={b} />
+
                 ))
             }
         </ol>
@@ -40,7 +16,6 @@ function Books_Section({ Books, Change_List, section_data }) {
 }
 Books_Section.prototype = {
     Books: PropTypes.array.isRequired,
-    Remove_Book: PropTypes.func.isRequired,
     Change_List: PropTypes.func.isRequired,
     section_data: PropTypes.string.isRequired
 }
