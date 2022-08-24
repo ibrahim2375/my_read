900e62d00d2a5eecf5b6cd914cc3d3f820022020
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-08-24 22:37:11 +0200

    up

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import PropTypes from 'prop-types'
 //router
